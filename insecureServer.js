c0f1f686dfa8a9ad86452413c3568eadbc837681
BugHunter-X <askzenai@gmail.com>
2025-01-16 13:12:08 +0100

    Add insecureServer.js file

---
+++ insecureServer.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
+console.log('Server is running on port 8080');
+//This server is vulnerable to a common attack vector that can be easily exploited, resulting in denial of service or data breaches.
